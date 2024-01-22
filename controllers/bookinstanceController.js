1e1cabe4cb58b533dcb8af9a1f108fe15caf2c29
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-01-22 15:21:59 -0800

    Implement delete book instance page

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -96,12 +96,25 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    res.redirect("/catalog/bookinstances");
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete Book Instance",
+    bookInstance: bookInstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  // Delete instance and redirect to all instances
+  await BookInstance.findByIdAndDelete(req.body.bookInstanceid);
+  res.redirect(`/catalog/bookinstances`);
 });
 
 // Display BookInstance update form on GET.
