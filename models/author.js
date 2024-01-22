ade165cabf0f3dc1e323b510c316ce367b75cd79
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-01-21 20:30:50 -0800

    Create inventory views

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -11,7 +12,8 @@ const AuthorSchema = new Schema({
 
 // Virtual for author's full name
 AuthorSchema.virtual("name").get(function () {
-  // Set blank full name in case author has no listed names
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
   let fullname = "";
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
@@ -20,8 +22,23 @@ AuthorSchema.virtual("name").get(function () {
   return fullname;
 });
 
-AuthorSchema.virtual("url").set(function () {
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
 
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+// Export model
 module.exports = mongoose.model("Author", AuthorSchema);
