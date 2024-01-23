8b5946360c102c1706f0dfa43316b10992e28ace
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-01-22 16:06:07 -0800

    Implement delete genre functionality

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -83,12 +83,45 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  // Get genre and all their books (in parallel)
+  const [genre, allBooksByGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (genre === null) {
+    // No results
+    res.redirect("/catalog/genres");
+  }
+
+  res.render("genre_delete", {
+    title: "Delete Genre",
+    genre: genre,
+    genre_books: allBooksByGenre,
+  });
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  const [genre, allBooksByGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (allBooksByGenre.length > 0) {
+    // Genre has books. Render in same was as for GET route
+    res.render("genre_delete", {
+      title: "Delete Author",
+      genre: genre,
+      genre_books: allBooksByGenre,
+    });
+    return;
+  } else {
+    // Genre has no books. Delete bokect and redirect to list of genres
+    console.log(genre._id);
+    await Genre.findOneAndDelete(genre._id);
+    res.redirect("/catalog/genres");
+  }
 });
 
 // Display Genre update form on GET.
