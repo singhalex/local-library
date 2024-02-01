9300e5dcb29c5d2694f4d1b910e66353c044c1d5
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-01-31 18:24:39 -0800

    Implement update genre form

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -124,7 +124,11 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  const genre = await Genre.findById(req.params.id);
+  res.render("genre_form", {
+    title: "Update Genre",
+    genre: genre,
+  });
 });
 
 // Handle Genre update on POST.
